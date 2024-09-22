420af5d3b9689cb944aaf88431a70c468f4de1a0
MichalRaszkowski <raszkowski17@gmail.com>
2024-09-22 13:54:11 +0200

    added jar file

--- src/ZipCompareByTime.java
+++ src/ZipCompareByTime.java
@@ -31,12 +31,12 @@ public class ZipCompareByTime {
             return Collections.emptyMap();
         }
     }
-    public static void compareTwoZips(File file1, File file2) {
+    public static void compareTwoZips(File file1, File file2, StringBuilder sb) {
         long date1 = file1.lastModified();
         long date2 = file2.lastModified();
 
         if(date1 != date2){
-            System.out.println("Files: " + file1.getName() + " and " + file2.getName() + " Have different modification dates.");
+            sb.append(file1.getName()).append("\n");
         }
     }
 
@@ -47,11 +47,16 @@ public class ZipCompareByTime {
         Map<String, File> map2 = chooseZipFiles();
         if(map2.isEmpty()) return;
 
+        StringBuilder resultMessage = new StringBuilder();
+
         for (String key : map1.keySet()) {
             if (map2.containsKey(key)) {
-                compareTwoZips(map1.get(key), map2.get(key));
+                compareTwoZips(map1.get(key), map2.get(key), resultMessage);
             }
         }
+        JOptionPane.showMessageDialog(null,
+                "Files: " + resultMessage.toString()
+                        + "have different modification dates","Comparison result", JOptionPane.INFORMATION_MESSAGE);
     }
 
     public static void main(String[] args) {
